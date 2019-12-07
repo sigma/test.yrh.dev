3b5a59cbe7ad3381a2232433004c1e4c09b1c937
Yann Hodique <yann.hodique@gmail.com>
2019-12-07 13:23:20 -0800

    refactor

--- src/vanity/vanity.go
+++ src/vanity/vanity.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"context"
+	"strings"
 	"text/template"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -16,20 +17,20 @@ var (
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	path := request.QueryStringParameters["path"]
+	repo := strings.Split(path, "/")[0]
 	tpl := `
 <!doctype html>
 <html>
   <head>
     <meta charset=utf-8>
-    <meta name=go-import content="{{.Vanity}}/{{.Path}} git https://github.com/{{.User}}/{{.Path}}">
-    <meta name=go-source content="{{.Vanity}}/{{.Path}} https://github.com/{{.User}}/{{.Path}} https://github.com/{{.User}}/{{.Path}}/tree/master{/dir} https://github.com/{{.User}}/{{.Path}}/blob/master{/dir}/{file}#L{line}">
-    <meta http-equiv=refresh content="0; url=https://github.com/{{.User}}/{{.Path}}">
+    <meta name=go-import content="{{.Vanity}}/{{.Repo}} git https://github.com/{{.User}}/{{.Repo}}">
+	<meta name=go-source content="{{.Vanity}}/{{.Repo}} https://github.com/{{.User}}/{{.Repo}} https://github.com/{{.User}}/{{.Repo}}/tree/master{/dir} https://github.com/{{.User}}/{{.Repo}}/blob/master{/dir}/{file}#L{line}">
   </head>
 </html>
 `
 
 	buf := new(bytes.Buffer)
-	obj := struct{ Vanity, User, Path string }{vanity, user, path}
+	obj := struct{ Vanity, User, Repo string }{vanity, user, repo}
 	if err := template.Must(template.New("goget").Parse(tpl)).Execute(buf, obj); err != nil {
 		return &events.APIGatewayProxyResponse{
 			StatusCode: 500,
